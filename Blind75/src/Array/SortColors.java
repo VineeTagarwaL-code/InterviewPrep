46bcf8b56da5774208436b9df705e1e3fb4857d9
Not-Your-Code <vineetagarwal.now@gmail.com>
2023-10-14 23:39:36 +0530

    added twoSum and did DNF algo

---
+++ Blind75/src/Array/SortColors.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/sort-colors/description/
+//DNF - [0...low-1] 0
+// [low ... mid-1] 1
+// [high+1 .... n-1]2
+public class SortColors {
+    public static void main(String[] args) {
+        int[] arr={0,0,1,2,1,0,0,1,2,0};
+        DNFalgoForSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void DNFalgoForSort(int[] arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+       while( mid <= high){
+           if(arr[mid] == 0){
+               swap(arr , low , mid);
+               low++; mid++;
+           }else if(arr[mid] == 1){
+               mid++;
+           }else{
+               swap(arr , mid , high );
+               high--;
+           }
+       }
+    }
+
+    static void swap(int[] nums , int i , int j){
+        int temp= nums[i];
+        nums[i] = nums[j];
+        nums[j]=temp;
+    }
+}
--- Blind75/src/Array/TwoSum.java
+++ Blind75/src/Array/TwoSum.java
@@ -6,9 +6,11 @@ import java.util.HashMap;
 //https://leetcode.com/problems/two-sum/
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {1,2,3};
         TwoSum obj = new TwoSum();
-        System.out.println(Arrays.toString(obj.twoSum(nums , 4)));
+//        System.out.println(Arrays.toString(obj.twoSum(nums , 4)));
+
+        System.out.println(Arrays.toString( optimal(nums , 5)));
     }
 //there can be multiple diff approaches to the same question
 //#1 use two for loop to check every combination
@@ -28,4 +30,33 @@ public class TwoSum {
         return new int[]{};
     }
 
+    static int[] Brute(int[] nums  , int target){
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i+1 ; j < nums.length ; j++){
+                if( i ==j) continue;
+                if(nums[i]+ nums[j] == target) return new int[]{i,j};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+//for better solution use hashmap
+    static int[] optimal(int[] nums , int target){
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int left = 0;
+        int right= nums.length-1;
+        while(left < right){
+            int sumation = nums[left] + nums[right];
+            if(sumation== target){
+                return new int[] {left , right};
+            }
+            else if( sumation < target ){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }

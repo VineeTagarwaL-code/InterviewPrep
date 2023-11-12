159f4209fbfd8862685d88b45540976c38056430
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2023-11-12 11:32:57 +0530

    added bubble sort

--- Algo_DataStructure/src/Algos/Sorting_Algo/BubbleSort.java
+++ Algo_DataStructure/src/Algos/Sorting_Algo/BubbleSort.java
@@ -1,4 +1,30 @@
 package Algos.Sorting_Algo;
 
+
+// |-----------------------------------------|
+// |  Time Complexity  | Space Complexity    |
+// |-----------------------------------------|
+// | Best  | O(1)      |    NONE             |
+// | Worst | O(Log2N)  |    NONE             |
+// |-----------------------------------------|
+
+
 public class BubbleSort {
+
+    public void BubbleSort(int[] arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr.length-1-i ; j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr , j , j+1);
+                }
+            }
+        }
+    }
+
+    public void swap(int[] arr , int first , int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last]=temp;
+    }
+
 }
--- Algo_DataStructure/src/Algos/Sorting_Algo/Main.java
+++ Algo_DataStructure/src/Algos/Sorting_Algo/Main.java
@@ -1,7 +1,13 @@
 package Algos.Sorting_Algo;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] arr = {5,4,3,2,1};
+
+        BubbleSort BS =  new BubbleSort();
+        BS.BubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
